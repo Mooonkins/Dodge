70e374473f50765dd32750f6f4f62edd8959ca8e
Mooonkins <Quasarjae72@gmail.com>
2023-04-25 12:45:38 +0900

    23/04/25

---
+++ Dodge/Assets/01.Scripts/BulletSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;    
+    public float fireRateMin = 0.5f;
+    public float fireRateMax = 3f;
+
+    private Transform target;    
+    private float spawnRate = 0f;
+    private float timeAfterSpawn;
+    
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+
+        spawnRate = Random.Range(fireRateMin, fireRateMax);
+
+        /*target = FindObjectOfType<PlayerController>().transform;*/
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+
+    }
+
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+
+        if (timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+            bullet.transform.LookAt(target);
+
+            spawnRate = Random.Range(fireRateMin, fireRateMax);
+        }        
+    }
+}
---
+++ Dodge/Assets/01.Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotateSpd = 60f;
+    
+    void Update()
+    {
+        transform.Rotate(0f, rotateSpd * Time.deltaTime, 0f);
+    }
+}
